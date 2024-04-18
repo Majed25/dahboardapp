f123b9c00d52fac61b8670b6313cc9793fda944e
majed25 <majid.elmenouar25@gmail.com>
2024-04-18 17:07:03 +0100

    Adding the http request handler

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from dashboard import *
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import http.server
+import socketserver
+from dashboard import dashboard
+
+class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            # Trigger the dashboard function to generate index.html
+            dashboard()
+            # Serve index.html as response
+            with open('index.html', 'rb') as file:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(file.read())
+        else:
+            # Serve other files (e.g., CSS, JS) as usual
+            super().do_GET()
 
 
 def main():
-    dashboard()
+    PORT = 8000  # You can adjust the port as needed
+    with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
+        print("Server started at http://localhost:%s" % PORT)
+        httpd.serve_forever()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
