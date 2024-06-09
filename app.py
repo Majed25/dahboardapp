f28426537a02171ac9ff407b8dc0a12ee3fcbdfb
majed25 <majid.elmenouar25@gmail.com>
2024-06-09 16:28:29 +0100

    use flask server

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import os
-from flask import Flask, jsonify
 from flask_caching import Cache
 import pandas as pd
-from dash import Input, Output, Dash
+from dash import Dash, Input, Output, callback
 import dash_bootstrap_components as dbc
 from dashboard import dashboard
 from app_layout import app_layout
@@ -10,31 +8,38 @@ from update_dashboard import update_dashboard
 
 
 # Initializing my Flask app
-server = Flask(__name__)
-cache = Cache(server, config={'CACHE_TYPE': 'simple'})
+#server = Flask(__name__)
 
+"""
 # Initialize Dash app
-app = Dash(__name__, server=server, url_base_pathname='/', external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, server=server, url_base_pathname='/', )
+"""
 dashboard_json = 'data/dashboard.json'
 dashboard_df = pd.read_json(dashboard_json)
 
 
+dash_app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash_app.server
+#cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+
 # Define the app layout
-app.layout = app_layout(dashboard_df)
+dash_app.layout = app_layout(dashboard_df)
 # Callback to update the bar chart and table based on league filter
-@app.callback(
+@callback(
     [Output('bar-chart', 'figure'),
      Output('data-table', 'data')],
     [Input('league-filter', 'value')]
 )
-@cache.cached(timeout=3600*4)
+#@cache.cached(timeout=3600*4)
 def update_layout(selected_league):
     print('Not serving cache')
     fig, data = update_dashboard(selected_league, dashboard_df)
     return fig, data
 
 # Webhook endpoint to refresh the dashboard
-@server.route('/refresh-dashboard', methods=['POST'])
+"""
+@server.route('/refresh_dashboard', methods=['POST'])
 def refresh_dashboard():
     global dashboard_df
     # Update the dashboard JSON file
@@ -43,11 +48,14 @@ def refresh_dashboard():
     # Clear the cache
     cache.clear()
     return jsonify({'status': 'success', 'message': 'Dashboard refreshed successfully'})
+"""
+
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    print('cache not served')
+    dashboard()
+    dash_app.run_server(debug=True)
 
 
 
